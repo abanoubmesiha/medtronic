81dd3844de9d648d5aae5bb152eb23612b252403
abanoubmesiha <anagy@app-corp.com>
2022-07-22 15:10:08 +0200

    Add header logos and responsiveness

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import './header.css';
 
 function Header() {
   return <header>
-    
+    <img src="/medtronic-logo.png" alt="medtronic-logo" className='logo' />
+    <img src="/medtronic-logo-with-text.png" alt="medtronic-logo-with-text" className='strapline' />
   </header>
 }
 
